cd82619ea4dd74129917b75feb7854e420f3943b
weston <wdrans@gmail.com>
2017-07-11 17:41:42 -0600

    remove debugger

--- app/controllers/projects_api_controller.rb
+++ app/controllers/projects_api_controller.rb
@@ -1,6 +1,5 @@
 class ProjectsApiController < ApplicationController
   def create
-    byebug
     project_params = params.require(:project).permit(:description, :title)
     project = Project.create!(project_params.merge(user: current_user))
     render json: project, status: :created
